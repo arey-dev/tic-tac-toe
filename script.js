c99cc89617eb589c2c43121b25ab02cd60cdb7fb
R.A Borja <raborja.dev@gmail.com>
2022-09-21 15:54:08 +0800

    recfactor whole script

--- script.js
+++ script.js
@@ -1,106 +1,92 @@
-// create gameboard module
-const Game = (() => {
-  const gameBoard = ['', '', '', '', '', '', '', '', '',];
+const Player = (name, mark) => {
+  return {
+    name,
+    mark,
+  }
+};
 
-  const Player = (mark) => {
-    const getMark = () => mark;
-  
-    return {
-      getMark
-    }
-  };
+const gameBoard = (() => {
+  const board = ['', '', '', '', '', '', '', '', '',];
 
-  // add players 
-  const player1= Player('X');
-  const player2 = Player('O');
+  const patterns = [
+    {pos1: 0, pos2: 1, pos3: 2}, 
+    {pos1: 3, pos2: 4, pos3: 5}, 
+    {pos1: 6, pos2: 7, pos3: 8}, 
+    {pos1: 0, pos2: 3, pos3: 6}, 
+    {pos1: 1, pos2: 4, pos3: 7}, 
+    {pos1: 2, pos2: 5, pos3: 8}, 
+    {pos1: 0, pos2: 4, pos3: 8}, 
+    {pos1: 2, pos2: 4, pos3: 6},
+  ];
 
-  let currentPlayer = player1; 
+  return {
+    board,
+    patterns,
+  }
+})();
 
-  // dom elements
-  const gameBoardElem = document.querySelector('#game-board');
+const game = (() => {
+  // game objects
+  const board = gameBoard.board;
+  const patterns = gameBoard.patterns;
+  let playerX;
+  let playerO;
+  let currentPlayer;
+
+  // cache dom
+  const boardElem = document.getElementById('game-board');
+  const startBtn = document.getElementById('start-btn');
+  const restartBtn = document.getElementById('restart-btn');
 
   // events
-  gameBoardElem.addEventListener('click', addMark);
+  startBtn.onclick = initGame;
+  boardElem.addEventListener('click', addMark);
+  boardElem.addEventListener('click', () => {
+    patterns.forEach(item => checkPattern(currentPlayer.mark, item))
+  });
+  boardElem.addEventListener('click', changePlayer);
 
-  // functions
+  // functions 
+  function initGame() {
+    // show board
+    boardElem.style.display = 'grid';
+
+    // show restart btn
+    restartBtn.style.display = 'inline-block';
+
+    // assign players 
+    playerX = Player('Player X', 'X');
+    playerO = Player('Player O', 'O');
+    
+    // assign current player
+    currentPlayer = playerX;
+  };
+
   function addMark(event) {
     const square = event.target;
     const pos = square.getAttribute('data-pos');
-    const mark = currentPlayer.getMark();
+    const mark = currentPlayer.mark;
 
-    if(gameBoard[pos] || square.tagName !== 'DIV') return;
+    if(board[pos] || square.tagName !== 'DIV') return;
 
     square.textContent = mark;
 
-    gameBoard[pos] = mark;
-
-
-    changePlayer();
-    checkWinner(mark);
+    board[pos] = mark;
   }
 
   function changePlayer() {
-    if(currentPlayer.getMark() === 'X') {
-      currentPlayer = player2;
+    if(currentPlayer.mark === 'X') {
+      currentPlayer = playerO;
     } 
     else {
-      currentPlayer = player1;
+      currentPlayer = playerX;
     }
   }
 
-  function checkWinner(mark) {
-    // horizontal pattern
-    if(gameBoard[0] === mark && gameBoard[1] === mark && gameBoard[2] === mark) {
+  const checkPattern = (mark, {pos1, pos2, pos3}) => {
+    if(board[pos1] === mark && board[pos2] === mark && board[pos3] === mark) {
       console.log(`${mark} winner`);
-      return;
-    }
-
-    if(gameBoard[3] === mark && gameBoard[4] === mark && gameBoard[5] === mark) {
-      console.log(`${mark} winner`);
-      return;
-    } 
-
-    if(gameBoard[6] === mark && gameBoard[7] === mark && gameBoard[8] === mark) {
-      console.log(`${mark} winner`);
-      return;
-    } 
-
-    // vertical pattern
-    if(gameBoard[0] === mark && gameBoard[3] === mark && gameBoard[6] === mark) {
-      console.log(`${mark} winner`);
-      return;
-    }
-
-    if(gameBoard[1] === mark && gameBoard[4] === mark && gameBoard[7] === mark) {
-      console.log(`${mark} winner`);
-      return;
-    } 
-
-    if(gameBoard[2] === mark && gameBoard[5] === mark && gameBoard[8] === mark) {
-      console.log(`${mark} winner`);
-      return;
-    } 
-
-    // cross pattern
-    if(gameBoard[0] === mark && gameBoard[4] === mark && gameBoard[8] === mark) {
-      console.log(`${mark} winner`);
-      return;
-    }    
-
-    if(gameBoard[2] === mark && gameBoard[4] === mark && gameBoard[6] === mark) {
-      console.log(`${mark} winner`);
-      return;
-    }
-    
-    // draw
-    if(gameBoard.every(val => val != '')) {
-      console.log("It's a draw");
-      return;
     }
   }
 
-  return {
-    currentPlayer,
-    gameBoard
-  };
 })();
